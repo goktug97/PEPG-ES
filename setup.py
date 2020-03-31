0d0d6dd265d31655a8da6a896bdd02b52916b6aa
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2020-03-31 18:38:42 +0300

    remove __pycache__ from dist

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pepg-es',
-      version='0.0.2',
+      version='0.0.3',
       description='Python Implementation of Parameter-exploring Policy Gradients Evolution Strategy',
       author='Göktuğ Karakaşlı',
       author_email='karakasligk@gmail.com',
